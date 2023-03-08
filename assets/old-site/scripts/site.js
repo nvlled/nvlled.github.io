235a562c786ff6b2c908dd074711fd5d7b6d5c2e
Ronald Casili <nvlled@gmail.com>
2023-03-08 14:14:50 +0800

    Remove files

--- assets/old-site/scripts/site.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("load", function() {
-    window.onhashchange = function(e) {
-        if (location.hash.substr(1) == "404") {
-            alert("resource was not found during content generation");
-            location.hash = "";
-        }
-    }
-});
