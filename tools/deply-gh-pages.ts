4fd0916071f4e9d018e6d6eb9b5bae175f1de7dd
Ronald Casili <nvlled@gmail.com>
2023-03-09 22:43:30 +0800

    Update page

--- tools/deply-gh-pages.ts
+++ tools/deply-gh-pages.ts
@@ -1,1 +1,8 @@
+#!/usr/bin/env -S deno run -A --unstable
+import $ from "https://deno.land/x/dax/mod.ts";
 
+await $`./cita.tsx build`;
+await $`git checkout build`;
+await $`rm -rf docs`;
+await $`mv _build docs`;
+await $`git checkout master`;
